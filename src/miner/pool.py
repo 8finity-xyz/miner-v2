f20f7b351bd282451c0a2c14879637ba5e6b9482
dev 888888888 <>
2025-06-08 15:39:10 +0300

    one session for poo

--- src/miner/pool.py
+++ src/miner/pool.py
@@ -20,6 +20,8 @@ class PoolMiner(BaseMiner):
         self.claim_info = None
         self.update_claim_info_task = asyncio.create_task(self._update_claim_info())
 
+        self.session = aiohttp.ClientSession(self.pool_url)
+
     async def get_problems(self):
         while True:
             new_problem = await self.problem_queue.get()
@@ -28,23 +30,22 @@ class PoolMiner(BaseMiner):
                 yield (0, *self.current_problem)
 
     async def _submit_solution(self, _, private_key_b):
-        async with aiohttp.ClientSession(self.pool_url) as session:
-            async with session.get(
-                "/submit",
-                params={
-                    "miner": self.miner_address,
-                    "private_key_b": hex(private_key_b)[2:],
-                },
-            ) as r:
-                if r.status != 200:
-                    self.logger.warning(f"Can't submit - status: {r.status}")
-                    self.logger.debug(f"/submit response - {await r.text()}")
-                    return
+        async with self.session.get(
+            "/submit",
+            params={
+                "miner": self.miner_address,
+                "private_key_b": hex(private_key_b)[2:],
+            },
+        ) as r:
+            if r.status != 200:
+                self.logger.warning(f"Can't submit - status: {r.status}")
+                self.logger.debug(f"/submit response - {await r.text()}")
+                return
 
-                data = await r.json()
-                await self.problem_queue.put(
-                    (int(data["private_key_a"]), int(data["difficulty"]))
-                )
+            data = await r.json()
+            await self.problem_queue.put(
+                (int(data["private_key_a"]), int(data["difficulty"]))
+            )
 
     async def flush_stats(self):
         self.logger.info("| STATS")
@@ -73,42 +74,38 @@ class PoolMiner(BaseMiner):
         self.logger.info(f"└ Pool url: {self.pool_url}")
 
     async def _poll_problem(self):
-        async with aiohttp.ClientSession(self.pool_url) as session:
-            while True:
-                async with session.get(
-                    "/problem", params={"miner": self.miner_address}
-                ) as r:
-                    if r.status != 200:
-                        self.logger.warning(f"Can't get problem - status: {r.status}")
-                        self.logger.debug(f"/problem response - {await r.text()}")
-                        continue
+        while True:
+            async with self.session.get(
+                "/problem", params={"miner": self.miner_address}
+            ) as r:
+                if r.status != 200:
+                    self.logger.warning(f"Can't get problem - status: {r.status}")
+                    self.logger.debug(f"/problem response - {await r.text()}")
+                    continue
 
-                    data = await r.json()
-                    await self.problem_queue.put(
-                        (int(data["private_key_a"]), int(data["difficulty"]))
-                    )
-                await asyncio.sleep(0.1)
+                data = await r.json()
+                await self.problem_queue.put(
+                    (int(data["private_key_a"]), int(data["difficulty"]))
+                )
+            await asyncio.sleep(0.1)
 
     async def _update_claim_info(self):
-        async with aiohttp.ClientSession(self.pool_url) as session:
-            while True:
-                async with session.get(
-                    "/claim", params={"miner": self.miner_address}
-                ) as r:
-                    if r.status != 200:
-                        self.logger.warning(
-                            f"Can't get claim info - status: {r.status}"
-                        )
-                        self.logger.debug(f"/claim response - {await r.text()}")
-                        continue
+        while True:
+            async with self.session.get(
+                "/claim", params={"miner": self.miner_address}
+            ) as r:
+                if r.status != 200:
+                    self.logger.warning(f"Can't get claim info - status: {r.status}")
+                    self.logger.debug(f"/claim response - {await r.text()}")
+                    continue
 
-                    data = await r.json()
-                    self.claim_info = {
-                        "pool_id": data["pool_id"],
-                        "miner": self.miner_address,
-                        "total_reward": int(data["total_reward"]),
-                        "signature": data["signature"],
-                    }
-                    with open("claim_info.json", "w") as f:
-                        json.dump(self.claim_info, f, indent=4)
-                await asyncio.sleep(60)
+                data = await r.json()
+                self.claim_info = {
+                    "pool_id": data["pool_id"],
+                    "miner": self.miner_address,
+                    "total_reward": int(data["total_reward"]),
+                    "signature": data["signature"],
+                }
+                with open("claim_info.json", "w") as f:
+                    json.dump(self.claim_info, f, indent=4)
+            await asyncio.sleep(60)
